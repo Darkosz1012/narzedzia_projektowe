829ad218c489a95acbe5271e769b02bd0c48a4fe
Dariusz <darek10121998db@gmail.com>
2021-01-14 22:35:27 +0100

    dockerfile and server

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 const func = require('./func');
 
-
 console.log(func());
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send(func())
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
