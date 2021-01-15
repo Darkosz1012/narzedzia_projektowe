8197132ef885038ccbeccf2a9ce54759c495ce06
Dariusz <darek10121998db@gmail.com>
2021-01-15 18:47:44 +0100

    failed test

--- func.test.js
+++ func.test.js
@@ -3,3 +3,6 @@ const func = require('./func');
 test('func returns Hello world', () => {
   expect(func()).toBe("Hello world");
 });
+test('func returns Hello world failed', () => {
+  expect(func()).toBe("Hello worldsdfsdf");
+});
