0f975fd98de872ce26cf548d81589ed4b99ea23e
Dariusz <darek10121998db@gmail.com>
2021-01-15 19:13:49 +0100

    repair tests

--- func.test.js
+++ func.test.js
@@ -3,6 +3,6 @@ const func = require('./func');
 test('func returns Hello world', () => {
   expect(func()).toBe("Hello world");
 });
-test('func returns Hello world failed', () => {
-  expect(func()).toBe("Hello worldsdfsdf");
-});
+// test('func returns Hello world failed', () => {
+//   expect(func()).toBe("Hello worldsdfsdf");
+// });
